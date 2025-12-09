856ff58f34147337ca23cadcf59ec9ad739c9a65
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-12-08 20:36:57 -0500

    dev

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,9 @@ import express from "express";
 
 // import CONFIG from "../config/config.js";
 import requireAuth from "./auth.js";
-import { displayMain, display401, display404, display500 } from "../controllers/display-controller.js";
-import { tgCommandControl } from "../controllers/data-controller.js";
-import { authController } from "../controllers/auth-controller.js";
+import { displayMain, display401, display404, display500 } from "../controllers/display-control.js";
+// import { tgCommandControl } from "../controllers/data-control.js";
+import { authController } from "../controllers/auth-control.js";
 
 const router = express.Router();
 
@@ -12,10 +12,10 @@ router.post("/site-auth-route", authController);
 router.get("/401", display401);
 
 //tg command sumbit
-router.post("/tg-submit-route", requireAuth, tgCommandControl);
+// router.post("/tg-submit-route", requireAuth, tgCommandControl);
 
 //tg display route
-router.get("/", requireAuth, tgDisplay);
+router.get("/", requireAuth, displayMain);
 
 router.use(display404);
 
