3c3f57a7b9cb77f60dd9ff51eb23a15fe1522534
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2026-01-30 10:56:09 -0500

    Dev

--- src/message.js
+++ src/message.js
@@ -1,7 +1,8 @@
 export const buildMessageInput = async (resumeText, jobInput) => {
   if (!jobInput) return null;
   const infoObj = await buildInfoObj();
-  if (!resumeText) return await buildMessageNoResume(jobInput);
+  if (!resumeText) return await buildMessageNoResume(jobInput, infoObj);
+
   return await buildMessageWithResume(resumeText, jobInput);
 };
 
@@ -25,132 +26,110 @@ export const buildInfoObj = async () => {
       },
       {
         jobId: 2,
-        company: "Federal Bureau of Investigation",
-        role: "Online Operator, Social Engineering",
-        timeframe: "2016 - Present",
-        bullets: [
-          "Operate online in support of investigations against various threat groups requiring substantial ideological expertise, independent judgement, and creativity",
-          "Identify and penetrate online locations and groups with ongoing threat activity",
-          "Disrupt physical threats by leveraging advanced subject matter expertise and technical capabilities",
+        company: process.env.COMPANY_2,
+        role: process.env.ROLE_2,
+        timeframe: process.env.TIMEFRAME_2,
+        bullets: [process.env.BULLETS_2_1, process.env.BULLETS_2_2, process.env.BULLETS_2_3],
+        accomplishments: [
+          process.env.ACCOMPLISHMENTS_2_1,
+          process.env.ACCOMPLISHMENTS_2_2,
+          process.env.ACCOMPLISHMENTS_2_3,
+          process.env.ACCOMPLISHMENTS_2_4,
         ],
       },
       {
         jobId: 3,
-        company: "Federal Bureau of Investigation",
-        role: "Cyber Threat Analyst, Criminal Actors",
-        timeframe: "May 2023 - December 2023",
-        bullets: [
-          "Supported and oversaw FBI investigations on cyber actors involved in various criminal violations, including ransomware, sim swapping, business email compromise, social engineering, etc.",
-          "Analyzed large criminal datasets to discover techniques indicative of nation state activity to focus resources",
-        ],
+        company: process.env.COMPANY_3,
+        role: process.env.ROLE_3,
+        timeframe: process.env.TIMEFRAME_3,
+        bullets: [process.env.BULLETS_3_1, process.env.BULLETS_3_2],
+        accomplishments: [process.env.ACCOMPLISHMENTS_3_1, process.env.ACCOMPLISHMENTS_3_2],
       },
       {
         jobId: 4,
-        company: "Federal Bureau of Investigation",
-        role: "Embedded Liaison Officer with USIC partner, Counterterrorism",
-        timeframe: "October 2021 - May 2023",
-        bullets: [
-          "Embedded with USIC partner working online CT threats to enable joint action and interagency collaboration",
-          "Operated independently to represent Bureau equities with OGA partner in complex and crowded target environments to ensure unified USIC approach and avoid operational conflicts",
-        ],
+        company: process.env.COMPANY_4,
+        role: process.env.ROLE_4,
+        timeframe: process.env.TIMEFRAME_4,
+        bullets: [process.env.BULLETS_4_1, process.env.BULLETS_4_2],
+        accomplishments: [process.env.ACCOMPLISHMENTS_4_1, process.env.ACCOMPLISHMENTS_4_2],
       },
       {
         jobId: 5,
-        company: "Federal Bureau of Investigation",
-        role: "Tactical Analyst / Program Manager, Counterterrorism",
-        timeframe: "July 2015 - October 2021",
-        bullets: [
-          "Provided detailed tactical analytic support to identify targets of interest and disrupt active terrorist plots",
-          "Managed and supported hundreds of counterterrorism investigations against multiple terrorist threat actors",
-        ],
+        company: process.env.COMPANY_5,
+        role: process.env.ROLE_5,
+        timeframe: process.env.TIMEFRAME_5,
+        bullets: [process.env.BULLETS_5_1, process.env.BULLETS_5_2],
       },
       {
         jobId: 6,
-        company: "Federal Bureau of Investigation",
-        role: "Presidential Daily Briefer for FBI Director and Attorney General",
-        timeframe: "2015-2018",
-        bullets: [
-          "Provided the Presidential Daily Briefing to the FBI Director, Attorney General, and senior FBI / DOJ executive management on approximately bimonthly basis",
-          "Developed in depth expertise of security challenges, nation state adversaries, and diverse security topics",
-        ],
+        company: process.env.COMPANY_6,
+        role: process.env.ROLE_6,
+        timeframe: process.env.TIMEFRAME_6,
+        bullets: [process.env.BULLETS_6_1, process.env.BULLETS_6_2],
       },
       {
         jobId: 7,
-        company: "Federal Bureau of Investigation",
-        role: "Social Media Threat Analyst, Counterterrorism",
-        timeframe: "September 2010 - July 2015",
-        bullets: [
-          "Managed and supported numerous FBI cases involving imminent jihadist threats to the Homeland",
-          "Analyzed, synthesized, and condensed large quantities of information to create tactical reports, analytical products, and strategic assessments for executive management",
-        ],
+        company: process.env.COMPANY_7,
+        role: process.env.ROLE_7,
+        timeframe: process.env.TIMEFRAME_7,
+        bullets: [process.env.BULLETS_7_1, process.env.BULLETS_7_2],
       },
     ],
 
     education: [
       {
-        school: "Catholic University of America",
-        program: "undergraduate",
-        degree1: "Bachelor of Arts in Political Science",
-        degree2: "Bachelor of Arts in History",
-        degree3: "Bachelor of Arts in Economics",
-        timeframe: "2006-2010",
-        graduation: "May 2010",
-        notes: "Triple major in Political Science, History, and Economics; graduated magna cum laude, 3.6 GPA; Phi Beta Kappa.",
+        school: process.env.SCHOOL_1,
+        program: process.env.PROGRAM_1,
+        degree1: process.env.DEGREE_1_1,
+        degree2: process.env.DEGREE_1_2,
+        degree3: process.env.DEGREE_1_3,
+        timeframe: process.env.TIMEFRAME_1,
+        graduation: process.env.GRADUATION_1,
+        notes: process.env.NOTES_1,
       },
       {
-        school: "Georgetown University",
-        degree: "Master of Arts in Security Studies",
-        program: "graduate",
-        timeframe: "2017-2019",
-        graduation: "May 2019",
-        notes: "International relations degree, focus on international security and current events.",
+        school: process.env.SCHOOL_2,
+        program: process.env.PROGRAM_2,
+        degree1: process.env.DEGREE_2_1,
+        timeframe: process.env.TIMEFRAME_2,
+        graduation: process.env.GRADUATION_2,
+        notes: process.env.NOTES_2,
       },
       {
-        certification: "GIAC Red Team Professional (GRTP)",
-        dateCertified: "September 2025",
-        program: "SANS Institute",
-        company: "Global Information Assurance Certification (GIAC)",
-        notes: "Red Team Professional certification, focus on red teaming and penetration testing.",
+        certification: process.env.CERTIFICATION_1,
+        dateCertified: process.env.DATE_CERTIFIED_1,
+        program: process.env.PROGRAM_1,
+        company: process.env.COMPANY_1,
+        notes: process.env.NOTES_1,
       },
       {
-        certification: "GIAC Certified Incident Handler (GCIH)",
-        dateCertified: "October 2024",
-        program: "SANS Institute",
-        company: "Global Information Assurance Certification (GIAC)",
-        notes: "Certified Incident Handler certification, focus on incident handling and response.",
+        certification: process.env.CERTIFICATION_2,
+        dateCertified: process.env.DATE_CERTIFIED_2,
+        program: process.env.PROGRAM_2,
+        company: process.env.COMPANY_2,
+        notes: process.env.NOTES_2,
       },
       {
-        certification: "GIAC Cyber Threat Intelligence (GCTI)",
-        dateCertified: "August 2023",
-        program: "SANS Institute",
-        company: "Global Information Assurance Certification (GIAC)",
-        notes: "Cyber Threat Intelligence certification, focus on threat intelligence and analysis.",
+        certification: process.env.CERTIFICATION_3,
+        dateCertified: process.env.DATE_CERTIFIED_3,
+        program: process.env.PROGRAM_3,
+        company: process.env.COMPANY_3,
+        notes: process.env.NOTES_3,
       },
       {
-        certification: "GIAC Security Essentials (GSEC)",
-        dateCertified: "July 2023",
-        program: "SANS Institute",
-        company: "Global Information Assurance Certification (GIAC)",
-        notes: "Security Essentials certification, focus on security fundamentals and best practices.",
+        certification: process.env.CERTIFICATION_4,
+        dateCertified: process.env.DATE_CERTIFIED_4,
+        program: process.env.PROGRAM_4,
+        company: process.env.COMPANY_4,
+        notes: process.env.NOTES_4,
       },
     ],
 
-    general: `I know the following programming languages well: VBA, JavaScript (NodeJs with Express and MongoDB). 
-          I have used the following programming languages but have less experience with them: Python, SQL, HTML, CSS (which I despise).
-          I am extremely proficient with Microsoft Excel and Access, and have extensive experience building advanced "full stack apps" with VBA in them. I am also experienced with PowerPoint, Word, etc.
-          I have used GitHub for about about 1 year and have used it heavily to store my projects. 
-          I have used Adobe Premier Pro, but I don't like to edit videos.
-          I can deploy webapps to my own VPS and have done this for a couple years, by setting up and maintaining the server. I often use Cloudflare for my DNS. 
-          I am extremely proficient with mobile messaging apps, especially Telegram. 
-          I am a news junkie, and extremely familiar with international political events and trends, with a deep understanding of the geopolitical landscape.
-          I am a student of history and have a deep knowledge of international cultures, and events. 
-          I am very good at analyzing information, by identifying and evaluating sources and routinely follow claims back to their original primary source. This is my greatest strength.
-          I know the recent history of the middle east, in particular the events of the Syrian Civil War, better than all but a handful of experts.
-          `,
+    general: process.env.GENERAL_INFO,
   };
 };
 
-export const buildMessageNoResume = async (jobInput) => {
+export const buildMessageNoResume = async (jobInput, infoObj) => {
   return [
     {
       role: "system",
@@ -177,6 +156,7 @@ export const buildMessageNoResume = async (jobInput) => {
           - "company" - The company I worked for at the job.
           - "timeframe" - The timeframe of the job.
           - "bullets" - An array of strings, each string representing a bullet point of the experience and achievements at the job.
+          - "accomplishments" - An array of strings, each string representing an accomplishment at the job.
         - "education" - An array of objects, each object representing my education and certifications.
         - "general" - General information about me and my skills.
   
@@ -206,7 +186,7 @@ export const buildMessageNoResume = async (jobInput) => {
       role: "user",
       content: `Here is the the Job Description: <job_description>${jobInput}</job_description>.
         
-        And here is the background information on me: <background_information>${JSON.stringify(OBJ)}</background_information>`,
+        And here is the background information on me: <background_information>${JSON.stringify(infoObj)}</background_information>`,
     },
   ];
 };
